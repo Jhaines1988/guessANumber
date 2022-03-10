293f6aae56707bfe1fb1790d3c7e34690f584906
Jhaines1988 <josephthaines@gmail.com>
2022-03-10 16:13:12 -0700

    Guess a number game logic

--- App.js
+++ App.js
@@ -1,20 +1,47 @@
-import { StatusBar } from 'expo-status-bar';
+import React, { useState } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
+import Header from './components/header';
+import StartGameScreen from './screens/StartGameScreen';
+import GameScreen from './screens/GameScreen';
+import GameOverScreen from './screens/GameOver';
 export default function App() {
+  const [userNumber, setUserNumber] = useState();
+  const [guessRounds, setGuessRounds] = useState(0);
+  const configureNewGame = () => {
+    setGuessRounds(0);
+    setUserNumber(null);
+  };
+  const gameOverHandler = (numOfRounds) => {
+    setGuessRounds(numOfRounds);
+  };
+
+  const startGameHandler = (selectedNumber) => {
+    setUserNumber(selectedNumber);
+  };
+
+  let content = <StartGameScreen onStartGame={startGameHandler} />;
+  if (userNumber && guessRounds <= 0) {
+    content = (
+      <GameScreen userChoice={userNumber} onGameOver={gameOverHandler} />
+    );
+  } else if (guessRounds > 0) {
+    content = (
+      <GameOverScreen
+        roundsNumber={guessRounds}
+        userNumber={userNumber}
+        restartGame={configureNewGame}
+      />
+    );
+  }
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={styles.screen}>
+      <Header title='Guess A Number!' />
+      {content}
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+  screen: { flex: 1 },
 });
---
+++ screens/GameOver.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { View, Text, StyleSheet, Button } from 'react-native';
+
+const GameOverScreen = (props) => {
+  return (
+    <View style={styles.screen}>
+      <Text>The Game is Over !</Text>
+      <Text>NumberOfRounds: {props.roundsNumber}</Text>
+      <Text>Number was : {props.userNumber}</Text>
+      <Button title='NEW GAME' onPress={props.restartGame} />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  screen: { flex: 1, justifyContent: 'center', alignItems: 'center' },
+});
+
+export default GameOverScreen;
